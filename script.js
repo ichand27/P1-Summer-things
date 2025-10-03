b42494e4cf31a917bedff73dab30823c58352eec
ichand27 <ichand27@aoiths.org>
2025-10-03 12:09:34 -0400

    october 1st

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let header = document.getElementById(header);
-let 
+let head = document.getElementsByClassName(h1);
+let clickTxt = document.getElemenstByClassName(clickTxt);
+let userInput = document.getElementById(userInput);
+let user = document.getElementById(user);
+let displayNme = document.getElementById(displayName);
+let footer = document.getElementById(footer);
