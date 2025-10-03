9578bf976131681eb794d80405c94dbcd52e2c30
ichand27 <ichand27@nycstudents.net>
2025-10-03 12:44:14 -0400

    october 3rd

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let header = document.getElementById(header);
+
+let headerPg = document.getElementById(header);
 let head = document.getElementsByClassName(h1);
 let clickTxt = document.getElemenstByClassName(clickTxt);
 let userInput = document.getElementById(userInput);
+let inputNm = document.getElementById(inputNm);
 let user = document.getElementById(user);
 let displayNme = document.getElementById(displayName);
 let footer = document.getElementById(footer);
+
+clickTxt.addEventListener("click", function () {
+
+        headerPg.style.display = none;
+        userInput.style.display = block;
+
+    });
+
